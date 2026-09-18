287d2d69197acc8897bfcd29e28f505d37ea8dee
LizaMariaEJ <lizamariaej8d@gmail.com>
2026-09-18 18:09:29 +0530

    Fix Render database initialization

--- app.py
+++ app.py
@@ -334,7 +334,9 @@ def init_db_command():
     init_db()
     print("Database initialized.")
 
-if __name__=="__main__":
-    with app.app_context():
+
+with app.app_context():
         init_db()
+        
+if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000,debug=False)
